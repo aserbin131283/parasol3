8ca0afecab4c44e02ccaf061b242758ef5c22918
aserbin131283 <aserbin131283@gmail.com>
2017-05-22 20:44:24 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	 $('.leng').click(function(){
+	 	$('.leng').toggleClass('active');
+	 });
+	 $('.enter').click(function(){
+	 	$('.background').css({
+	 		'background-image':'url(images/background-man-hover.jpg)'});
+	 });
+	 $('.menu-toggle').click(function(){
+	 	$('.header-menu-right').toggleClass('active');
+	 });
+})
+
+
+
